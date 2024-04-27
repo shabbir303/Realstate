a079d8f2b5bf4327f8092483cc1a24a44e4d49ac
shabbir <hossainshabbir51@gmail.com>
2024-04-27 22:52:50 +0600

    1st commit

---
+++ app.js
@@ -0,0 +1,20 @@
+import express from "express";
+import postRoute from "./route/post.route.js";
+import authRoute from "./route/auth.route.js";
+
+const app = express();
+
+
+// console.log("test");
+
+app.use("/api/test",(req,res)=>{
+  res.send("it works");
+})
+
+app.use("/api/posts", postRoute);
+
+app.use("/api/auth", authRoute);
+
+app.listen(8800, ()=>{
+  console.log("server is running on port 8800");
+})
---
+++ controllers/auth.controllers.js
@@ -0,0 +1,9 @@
+export const register = (req,res)=>{
+  //db changes
+}
+export const login = (req,res)=>{
+  //db changes
+}
+export const logout = (req,res)=>{
+  //db changes
+}
